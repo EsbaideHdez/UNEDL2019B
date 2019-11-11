fa15a5144e832ac55634c6ce70f5fbcc1ae86fc7
esbaide <esbaide_hdez@hotmail.com>
2019-11-11 09:03:46 -0600

    Primer programa visual o ventanas visuales

---
+++ Windows/UsingPrintHelper/UsingPrintHelper/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingPrintHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About about = new About();
+            about.MdiParent = this; //indica que es la clase padre y solo esta dentro del frame
+            about.Show();
+        }
+
+        private void printHelperToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintHelper ph = new PrintHelper();
+            ph.MdiParent = this;
+            ph.Show();
+        }
+    }
+}
