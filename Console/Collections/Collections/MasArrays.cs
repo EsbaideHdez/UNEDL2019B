0c0580609553cc7068c02f4398a82dd36d74228e
esbaide <esbaide_hdez@hotmail.com>
2019-11-18 20:50:30 -0600

    Collections

---
+++ Console/Collections/Collections/MasArrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class MasArrays
+    {
+        public void Sorted()
+        {
+            SortedList sl = new SortedList();
+            sl.Add(1, "Valor 1");
+            sl.Add(2, "Valor 2");
+            sl.Add(3, "Valor 3");
+            ICollection llaves = sl.Keys;
+            foreach(var k in llaves)
+            {
+                Console.WriteLine("llaves:" + k + "valor " + sl[k]);
+            }
+        }
+    }
+}
---
+++ Console/Collections/Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Arrays test = new Arrays();
+                //test.Arreglo1();
+                MasArrays test2 = new MasArrays();
+                ttest2.Sorted();
+                ArrayList al = new ArrayList();
+                al.Add(null);
+                al.Add("string");
+                al.Add(1);
+                al.Add()
+                foreach (var i in al)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        
+    }
+}
