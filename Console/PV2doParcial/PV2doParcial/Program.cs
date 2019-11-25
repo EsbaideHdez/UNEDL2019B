2842537ff5917deadc51e02587d2726994f85e7e
esbaide <esbaide_hdez@hotmail.com>
2019-11-25 10:53:07 -0600

    examen 2o parcial ejercicio de c

---
+++ Console/PV2doParcial/PV2doParcial/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV2doParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack p = new Stack();
+            Console.WriteLine("Ingresa los signos parentesis, llaves y  corchtes ");
+               string n = Console.ReadLine();
+         
+            for (int i = 0; i < n.Length; i++)
+            {
+                if ((n[i] == '(') || (n[i] == '{') || (n[i] == '['))
+                {
+                    p.Push(n[i]);
+                }
+                else if (p.Count > 0)
+                {
+                    switch (n[i])
+                    {
+                        case ']'://caso ]
+
+                            p.Pop();
+                            break;
+                        case '}'://caso}
+
+                            p.Pop();
+                            break;
+                        case ')'://caso )
+                            p.Pop();
+                            break;
+                    }
+                }
+            }
+            if (p.Count == 0)
+            {
+                Console.WriteLine("LA ECUACION ESTA CORRECTAMENTE EQUILIBRADA");//imprime es correcta
+            }
+            else
+            {
+                Console.WriteLine("LA ECUACION NO ECUACION NO ESTA EQUILIBRADA");//imprime no esta equilibrada
+            }
+        }
+    }
+}
+
+
