1aa53f3c6ab660ea20ce323671535dc2678c4166
esbaide <esbaide_hdez@hotmail.com>
2019-11-27 10:13:59 -0600

    examen de taller

---
+++ Windows/Examen_TPV2doParcial/Examen_TPV2doParcial/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Examen_TPV2doParcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.filename = new System.Windows.Forms.TextBox();
+            this.read_button1 = new System.Windows.Forms.Button();
+            this.write_button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.content = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Entrada";
+            // 
+            // filename
+            // 
+            this.filename.Location = new System.Drawing.Point(121, 31);
+            this.filename.Name = "filename";
+            this.filename.Size = new System.Drawing.Size(163, 20);
+            this.filename.TabIndex = 1;
+            // 
+            // read_button1
+            // 
+            this.read_button1.Location = new System.Drawing.Point(315, 31);
+            this.read_button1.Name = "read_button1";
+            this.read_button1.Size = new System.Drawing.Size(75, 23);
+            this.read_button1.TabIndex = 2;
+            this.read_button1.Text = "Leer";
+            this.read_button1.UseVisualStyleBackColor = true;
+            // 
+            // write_button2
+            // 
+            this.write_button2.Location = new System.Drawing.Point(415, 31);
+            this.write_button2.Name = "write_button2";
+            this.write_button2.Size = new System.Drawing.Size(75, 23);
+            this.write_button2.TabIndex = 3;
+            this.write_button2.Text = "Escribir";
+            this.write_button2.UseVisualStyleBackColor = true;
+            this.write_button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contenido";
+            // 
+            // content
+            // 
+            this.content.Location = new System.Drawing.Point(61, 86);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(303, 20);
+            this.content.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 145);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.write_button2);
+            this.Controls.Add(this.read_button1);
+            this.Controls.Add(this.filename);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox filename;
+        private System.Windows.Forms.Button read_button1;
+        private System.Windows.Forms.Button write_button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox content;
+    }
+}
+
---
+++ Windows/Examen_TPV2doParcial/Examen_TPV2doParcial/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen_TPV2doParcial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (filename.Text != "")
+            {
+                content.Text = "";
+                try
+                {
+                    var fs = File.OpenRead(filename.Text);
+                    var stream = new StreamReader(fs);
+                    String line;
+                    while ((line = stream.ReadLine()) != null)
+                    {
+                        content.Text += line + "\n";
+                    }
+                    fs.Close();
+                }
+                catch (FileNotFoundException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debes escribir el nombre de un archivo!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (filename.Text != "")
+            {
+                try
+                {
+                    File.WriteAllText(filename.Text, content.Text);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debes escribir el nombre de un archivo!");
+            }
+        }
+    }
+}
+
+/*Esbaide Yaziel Hernández Delgadillo   27-Noviembre-2019
+ * 1. Describa en sus propias palabras el concepto de colecciones (collections) y cuales tipos existen.
+ * R=son las clases que se utilizan para el manejo de varios valores de diferentes seriesy existen 2 tipos las
+ * non-generic collections y las generic collections.
+ * 
+2.Defina a que se refiere el concepto de código no seguro y que se necesita para implementarlo.
+R=el codigo no seguro es el que usa los apuntadores y se le llama contexto inseguro y para implementarlo es utilizando
+el modificador unsafe para un metodo o constructor, tambien se deve utilizar la palabra unsafe para definir el grupo de 
+codigo y  se debe habilitar en el compilador el uso del unsafe ya que si este no se habilita se genera un error a la hora de compilar.
+
+3.Mencione al menos tres clases que se utilizan en I/O y proporcione brevemente la idea central de las mismas.
+FileStream:lee y escribe bytes desde y hacia un archivo
+BufferedStream: lee y escribe bytes desde y hacia otros Streams para mejorar el rendimiento de las operaciones de entrada y salia
+PipeStream: lee y escribe bytes desde y hacia diferentes procesos.
+
+4. Explique la diferencia entre los siguientes códigos en csharp
+a. Console. Writeline ("resultado: (*ptr) ").
+b. Console.WriteLine("resultado: ((int)ptr)")
+5.Mencione al menos tres caracteristicas de las excepciones (Exceptions).
+R=las excepciones son eventos inesperados que aparecen durante la ejecucion de agun programa,
+puede ser al tratar de acceder a un elemento fuera del limite.
+
+6.En Java, mencione y describa los tres tipos de excepciones.
+R=
+7. Explique brevemente a que se refiere con directivas para preprocesador y que se necesita para
+usarlas.
+R=son las que dan las instrucciones al compilador para que procese la informacion antes de iniciar con
+la propia compilacion y para usarlas es que las directivas inician con # y no termina con , las directivas deben ser las unicas
+instrucciones de la linea y el compilador no contiene un prepocesador
+
+8. Explique las diferencias entre ArrayList, SortedList y  HashTable.
+R=el ArrayList es el que Almacena objetos de cualquier tipo a manera de arreglo. No se necesita especificar el tamaño del arreglo, ya que se
+incrementa automáticamente, el SortedList es el que acomoda automaticamente los elemtos deascendente y el HashTable es el que recupera los valores
+utilizando la llave dada.
+
+9. Describa las para que sirven StreamReader y StreamWriter
+R=son las clases de apoyp que permiten la lectura y la escritura convirtiendo los bytes en caracteres y viceversa.
+
+10. Explique el funcionamiento del algoritmo Quicksort
+R=es un algoritmo que inicia partiendo de un pivote partiendo la lista de los elementos en 2, los elemntos de la seccion demla derecha so mayores
+y los del lado izquierdo menores y se hace el mismo procediemto en la parte derecha y en la parte izquierda y se puede utilizar la recursividad.
+
+11. En Java, explique cuál es la diferencia entre System.out, System.in y System.err
+R=system.out es la salida de normas. el system.in es la entradda de normas y el system.err es el error de las normas.
+
+12. Explique cuáles son las ventajas de utilizar "generic collections"
+R=pueden almacenar cualquier tipo de elementos, la limitantes es quue debe usar casting para poder recorrer el contenido ya que si no se hace se genera una
+excepcion
+
+13 Desarrolle en C#, un programa en Windows Forms llamado TPV2doParcial que permita crear un
+archivo, lo lea y permita escribir en el. El programa debe permitir la captura de una lista de palabras
+y al finalizar la captura, debe reportar la cantidad total de palabras, las palabras repetidas y la
+cantidad de ocurrencias. Capture las excepciones necesarias. Al terminar, suba su codigo a su
+repositorio y escriba el SHA correspondiente en el examen*/
